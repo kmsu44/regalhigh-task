4a3b42042c3fff4aab486c4f78fd81fedf943d26
kmsu44 <kmsu444@gmail.com>
2024-06-13 21:02:01 +0900

    feat: i18n 기능 추가

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { getMessages } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "regal-task",
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const messages = await getMessages();
+  return (
+    <html lang={locale}>
+      <head />
+      <body className={inter.className}>
+        <NextIntlClientProvider messages={messages}>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            <Header />
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/trade/[coin]/page.tsx
@@ -0,0 +1,10 @@
+export default function Page({
+  params,
+}: {
+  params: {
+    lang: string;
+    coin: string;
+  };
+}) {
+  return <div>Trade</div>;
+}
