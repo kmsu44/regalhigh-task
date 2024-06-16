53fb1b23859f78b5974d92bdd479ef3ca621c49b
kmsu44 <kmsu444@gmail.com>
2024-06-16 16:50:41 +0900

    feat: middleware 설정
    - coin정보 없을때 기본 BTCUSDT으로 라우트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
-import createMiddleware from "next-intl/middleware";
+import createIntlMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware({
-  locales: ["en", "ko"],
-  defaultLocale: "en",
-});
+export default async function middleware(request: NextRequest) {
+  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
+
+  if (locale != null && segments.join("/") === "trade") {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = `/${locale}/trade/BTCUSDT`;
+    return NextResponse.redirect(new URL(redirectUrl.href).toString());
+  }
+  const handleI18nRouting = createIntlMiddleware({
+    locales: ["en", "ko"],
+    defaultLocale: "en",
+  });
+  const response = handleI18nRouting(request);
+  return response;
+}
 
 export const config = {
-  matcher: ["/", "/(en|ko)/:path*"],
+  matcher: ["/", "/(ko|en)/:path*", "/(ko|en)/trade", "/trade"],
 };
