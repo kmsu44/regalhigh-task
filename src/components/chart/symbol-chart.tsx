a9215b56b39b57d845e0614569283ff12c894849
kmsu44 <kmsu444@gmail.com>
2024-06-18 05:08:36 +0900

    perf: 차트 데이터 웹 소켓 연결 성능 개선

--- src/components/chart/symbol-chart.tsx
+++ src/components/chart/symbol-chart.tsx
@@ -2,22 +2,21 @@
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { init, dispose } from "klinecharts";
+import { init, dispose, KLineData } from "klinecharts";
 import { Interval } from "@binance/connector-typescript";
 import { klinesOptions } from "@/lib/query-options";
-import useKlinesSubscription from "@/hooks/use-klines-subscription";
 import { useKlineIntervalState } from "@/atoms/klines/kline-interval-atom";
 import { getLotSizeBySymbol, getTickSizeBySymbol } from "@/lib/utils";
+import { getQueryClient } from "@/lib/get-query-client";
+
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET || "";
 
 export default function SymbolChart({ symbol }: { symbol: ExchangeInfo }) {
   const [klineInterval] = useKlineIntervalState();
   const { data } = useSuspenseQuery(
     klinesOptions({ symbol: symbol.symbol, interval: Interval[klineInterval] })
   );
-  const k = useKlinesSubscription({
-    symbol: symbol.symbol.toLowerCase(),
-    interval: Interval[klineInterval],
-  });
+  const queryClient = getQueryClient();
   const { tickSizeLength } = getTickSizeBySymbol({
     symbol: symbol,
   });
@@ -26,15 +25,50 @@ export default function SymbolChart({ symbol }: { symbol: ExchangeInfo }) {
   });
   useEffect(() => {
     if (!data) return;
-    const chart = init("chart", {
-      locale: "ko",
-    });
+    const chart = init("chart");
+    const websocket = new WebSocket(
+      `${SOCKET_URL}/ws/${symbol.symbol.toLowerCase()}@kline_${
+        Interval[klineInterval]
+      }`
+    );
+    websocket.onopen = () => {
+      console.log("connected");
+    };
+    websocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      const newData = {
+        timestamp: data.k.t,
+        open: data.k.o,
+        high: data.k.h,
+        low: data.k.l,
+        close: data.k.c,
+        volume: data.k.v,
+        closeTime: data.k.c,
+        quoteVolume: data.k.q,
+        numberOfTrades: data.k.n,
+        takerBuyBaseAssetVolume: data.k.V,
+        takerBuyQuoteAssetVolume: data.k.Q,
+      };
+      chart?.updateData(newData);
+      queryClient.setQueryData(
+        ["klines", symbol.symbol, klineInterval],
+        (oldData: KLineData[]) => {
+          if (oldData[oldData.length - 1].timestamp === newData.timestamp) {
+            oldData[oldData.length - 1] = newData;
+            return oldData;
+          }
+          const newKlines = [...oldData, newData];
+          return newKlines;
+        }
+      );
+    };
     chart?.setPriceVolumePrecision(tickSizeLength, lotSizeLength);
-    chart?.applyNewData(data);
+    chart?.applyNewData(data, true);
     return () => {
       dispose("chart");
+      websocket.close();
     };
-  }, [data, tickSizeLength, lotSizeLength]);
+  }, [klineInterval, symbol]);
 
   return <div id="chart" className="w-full h-[576px]" />;
 }
