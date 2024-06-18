bcc398ce04c6b83166354bb41755e71aecf7aeac
kmsu44 <kmsu444@gmail.com>
2024-06-18 23:06:42 +0900

    test: 나머지 컴포넌트 test

--- src/app/[locale]/trade/[coin]/page.tsx
+++ src/app/[locale]/trade/[coin]/page.tsx
@@ -2,7 +2,7 @@ import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import Market from "@/components/market/market";
 import { getQueryClient } from "@/lib/get-query-client";
 import { symbolsOptions, tickersOptions } from "@/lib/query-options";
-// import CoinInfo from "@/components/info/coin-info";
+import CoinInfo from "@/components/info/coin-info";
 
 export default async function Page({
   params,
@@ -11,16 +11,16 @@ export default async function Page({
     coin: string;
   };
 }) {
-  // const symbol = params.coin.toUpperCase();
+  const symbol = params.coin.toUpperCase();
   const queryClient = getQueryClient();
-  // await queryClient.prefetchQuery(symbolsOptions);
-  // void queryClient.prefetchQuery(tickersOptions);
+  await queryClient.prefetchQuery(symbolsOptions);
+  void queryClient.prefetchQuery(tickersOptions);
   return (
     <div className="flex bg-background w-full">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        {/* {symbol && <CoinInfo symbol={params.coin.toUpperCase()} />} */}
+        {symbol && <CoinInfo symbol={params.coin.toUpperCase()} />}
         <div className="h-[357px] max-w-80 flex-shrink-0">
-          {/* <Market /> */}
+          <Market />
           <div>트레이드</div>
         </div>
       </HydrationBoundary>
