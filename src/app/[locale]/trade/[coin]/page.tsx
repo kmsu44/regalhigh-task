41d26324750422d1066f2d75d90e4e175f322972
kmsu44 <kmsu444@gmail.com>
2024-06-19 02:02:23 +0900

    test: staticparams 적용

--- src/app/[locale]/trade/[coin]/page.tsx
+++ src/app/[locale]/trade/[coin]/page.tsx
@@ -3,17 +3,24 @@ import Market from "@/components/market/market";
 import { getQueryClient } from "@/lib/get-query-client";
 import { symbolsOptions, tickersOptions } from "@/lib/query-options";
 import CoinInfo from "@/components/info/coin-info";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getSymbols } from "@/api/market-api";
+
+export async function generateStaticParams() {
+  const coins = await getSymbols();
+  return coins.map((coin) => ({
+    params: {
+      coin: coin.symbol,
+    },
+  }));
+}
 
 export default async function Page({
   params,
 }: {
   params: {
-    locale: string;
     coin: string;
   };
 }) {
-  unstable_setRequestLocale(params.locale);
   const symbol = params.coin.toUpperCase();
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(symbolsOptions);
