de52b71fe289cb12eeb4172fa0854f40b376d2ad
kmsu44 <kmsu444@gmail.com>
2024-06-18 10:31:31 +0900

    test: 미들웨어 테스트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,37 @@
-import createIntlMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
+// import createIntlMiddleware from "next-intl/middleware";
+// import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(request: NextRequest) {
-  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
+// export default async function middleware(request: NextRequest) {
+//   const [, locale, ...segments] = request.nextUrl.pathname.split("/");
 
-  if (locale != null && segments.join("/") === "trade") {
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.pathname = `/${locale}/trade/BTCUSDT`;
-    return NextResponse.redirect(new URL(redirectUrl.href).toString());
-  }
-  const handleI18nRouting = createIntlMiddleware({
-    locales: ["en", "ko"],
-    defaultLocale: "en",
-  });
-  const response = handleI18nRouting(request);
-  return response;
-}
+//   if (locale != null && segments.join("/") === "trade") {
+//     const redirectUrl = request.nextUrl.clone();
+//     redirectUrl.pathname = `/${locale}/trade/BTCUSDT`;
+//     return NextResponse.redirect(new URL(redirectUrl.href).toString());
+//   }
+//   const handleI18nRouting = createIntlMiddleware({
+//     locales: ["en", "ko"],
+//     defaultLocale: "en",
+//   });
+//   const response = handleI18nRouting(request);
+//   return response;
+// }
+
+// export const config = {
+//   matcher: ["/", "/(ko|en)/:path*", "/(ko|en)/trade", "/trade"],
+// };
+
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ["en", "ko"],
+
+  // Used when no locale matches
+  defaultLocale: "en",
+});
 
 export const config = {
-  matcher: ["/", "/(ko|en)/:path*", "/(ko|en)/trade", "/trade"],
+  // Match only internationalized pathnames
+  matcher: ["/", "/(ko|en)/:path*"],
 };
