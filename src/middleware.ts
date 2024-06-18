7e182bdd7ff43a725314530a4bcabd1b4570179f
kmsu44 <kmsu444@gmail.com>
2024-06-18 18:54:05 +0900

    test : middleware 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,10 @@
-import createIntlMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from "next-intl/middleware";
 
-export default async function middleware(request: NextRequest) {
-  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
-
-  if (locale != null && segments.join("/") === "trade") {
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.pathname = `/${locale}/trade/BTCUSDT`;
-    return NextResponse.redirect(new URL(redirectUrl.href).toString());
-  }
-  const handleI18nRouting = createIntlMiddleware({
-    locales: ["en", "ko"],
-    defaultLocale: "en",
-  });
-  const response = handleI18nRouting(request);
-  return response;
-}
+export default createMiddleware({
+  locales: ["en", "ko"],
+  defaultLocale: "en",
+});
 
 export const config = {
-  matcher: ["/", "/(ko|en)/:path*", "/(ko|en)/trade", "/trade"],
+  matcher: ["/", "/(ko|en)/:path*"],
 };
