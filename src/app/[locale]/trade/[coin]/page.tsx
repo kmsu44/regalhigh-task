8d1d1047ad3948069a1f9044ed71ece089c3436f
kmsu44 <kmsu444@gmail.com>
2024-06-18 23:01:37 +0900

    test: 라우트 확인을 위한

--- src/app/[locale]/trade/[coin]/page.tsx
+++ src/app/[locale]/trade/[coin]/page.tsx
@@ -12,16 +12,16 @@ export default async function Page({
     coin: string;
   };
 }) {
-  const symbol = params.coin.toUpperCase();
+  // const symbol = params.coin.toUpperCase();
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(symbolsOptions);
-  void queryClient.prefetchQuery(tickersOptions);
+  // await queryClient.prefetchQuery(symbolsOptions);
+  // void queryClient.prefetchQuery(tickersOptions);
   return (
     <div className="flex bg-background w-full">
       <HydrationBoundary state={dehydrate(queryClient)}>
         {/* {symbol && <CoinInfo symbol={params.coin.toUpperCase()} />} */}
         <div className="h-[357px] max-w-80 flex-shrink-0">
-          <Market />
+          {/* <Market /> */}
           <div>트레이드</div>
         </div>
       </HydrationBoundary>
